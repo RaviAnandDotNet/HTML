bf1abd1436997f94ec07abeeb7f3d06820ec3654
RaviAnandDotNet <anandravi40@gmail.com>
2024-04-17 00:49:18 +0530

    update

---
+++ GoelSonsFabric/js/site.js
@@ -0,0 +1,47 @@
+$('.nav-fabrics').hover(function(){
+    $('.dropdown-menu').toggleClass('show')
+})
+$('.viscose-jacquard').hover(function(){
+    $(".nav-fabric-image").toggleClass("d-none")
+    $(".nav-fabric-image").find("img").attr("src","/image/navimage/jpeg-optimizer_image.png")
+})
+$('.viscose-stripe').hover(function(){
+    $(".nav-fabric-image").toggleClass("d-none")
+    $(".nav-fabric-image").find("img").attr("src","/image/navimage/jpeg-optimizer_image2.png")
+})
+$('.viscose-slub').hover(function(){
+    $(".nav-fabric-image").toggleClass("d-none")
+    $(".nav-fabric-image").find("img").attr("src","/image/navimage/jpeg-optimizer_image3.png")
+})
+$('.viscose-plain').hover(function(){
+    $(".nav-fabric-image").toggleClass("d-none")
+    $(".nav-fabric-image").find("img").attr("src","/image/navimage/jpeg-optimizer_image4.png")
+})
+$('.viscose-dobby').hover(function(){
+    $(".nav-fabric-image").toggleClass("d-none")
+    $(".nav-fabric-image").find("img").attr("src","/image/navimage/jpeg-optimizer_image5.png")
+})
+$('.viscose-crepe').hover(function(){
+    $(".nav-fabric-image").toggleClass("d-none")
+    $(".nav-fabric-image").find("img").attr("src","/image/navimage/jpeg-optimizer_image6.png")
+})
+$('.viscose-boota').hover(function(){
+    $(".nav-fabric-image").toggleClass("d-none")
+    $(".nav-fabric-image").find("img").attr("src","/image/navimage/jpeg-optimizer_image7.png")
+})
+$('.viscose-lurex').hover(function(){
+    $(".nav-fabric-image").toggleClass("d-none")
+    $(".nav-fabric-image").find("img").attr("src","/image/navimage/jpeg-optimizer_image8.png")
+})
+$('.viscose-georgette').hover(function(){
+    $(".nav-fabric-image").toggleClass("d-none")
+    $(".nav-fabric-image").find("img").attr("src","/image/navimage/jpeg-optimizer_image9.png")
+})
+$('.cotton-jacquard').hover(function(){
+    $(".nav-fabric-image").toggleClass("d-none")
+    $(".nav-fabric-image").find("img").attr("src","/image/navimage/jpeg-optimizer_image10.png")
+})
+$('.viscose-check').hover(function(){
+    $(".nav-fabric-image").toggleClass("d-none")
+    $(".nav-fabric-image").find("img").attr("src","/image/navimage/jpeg-optimizer_image11.png")
+})
